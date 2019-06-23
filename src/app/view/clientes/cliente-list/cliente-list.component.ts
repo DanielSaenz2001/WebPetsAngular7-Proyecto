6a084279454fe09972b8321c82b3c0722f0372e8
DanielSaenz2001 <daniel.saenz@upeu.edu.pe>
2019-06-22 20:32:52 -0500

    cliente bus

--- src/app/view/clientes/cliente-list/cliente-list.component.ts
+++ src/app/view/clientes/cliente-list/cliente-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../../../models/cliente';
 import { ClientesService } from '../../../service/clientes.service';
 import { element } from '../../../../../node_modules/@angular/core/src/render3';
+import  Swal  from 'sweetalert2'
+import { type } from 'os';
 
 @Component({
   selector: 'app-cliente-list',
@@ -10,16 +12,51 @@ import { element } from '../../../../../node_modules/@angular/core/src/render3';
 })
 export class ClienteListComponent implements OnInit {
   clienteList: Cliente[];
+
+  buscar:string;
   constructor(private clientesService:ClientesService) { }
 
   ngOnInit() {
-    return this.clientesService.getClientes().snapshotChanges().subscribe(item=>{
+    this.mostrarclientes();
+  }
+  mostrarclientes(){
+  	this.clientesService.getClientes().snapshotChanges().subscribe(item=>{
       this.clienteList=[];
       item.forEach(element=>{
         let x=element.payload.toJSON();
         x["$key"]=element.key;
         this.clienteList.push(x as Cliente);
+      });
+    });
+  }
+  consuCliente(){
+  	this.clientesService.getClientes().snapshotChanges().subscribe(item=>{
+      this.clienteList=[];
+      item.forEach(element=>{
+        let x=element.payload.toJSON();
+        x["$key"]=element.key;
+        this.clienteList.push(x as Cliente);
+      });
+      this.clienteList=this.clienteList.filter(data=>{
+    	  return data.dni.toString().trim()===this.buscar;
       })
+      if(this.clienteList.length===0){
+        Swal.fire({
+          position:'top-end',
+          type: "success",
+          title: 'Cliente No Encontrado',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }else{
+        Swal.fire({
+          position:'top-end',
+          type: "success",
+          title: 'Cliente Encontrado',
+          showConfirmButton: false,
+          timer: 1500
+      })
+    }
     })
   }
 
